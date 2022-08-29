5a52fffb1a7a4d2dc8ea8e7561d7d363f076cd2e
Aarnee <daniil.belianin@gmail.com>
2022-08-29 12:59:02 +0300

    Initial commit

---
+++ test_main_page.py
@@ -0,0 +1,12 @@
+import time
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_search_button_add_to_cart(browser):
+    browser.get(link)
+    time.sleep(10)
+    browser.implicitly_wait(10)
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert button, "Искомый элемент не найден"
