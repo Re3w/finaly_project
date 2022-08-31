24a7c73a328ca1cb465b9a6b0139bcf70e320470
Aarnee <daniil.belianin@gmail.com>
2022-08-31 11:25:22 +0300

    Modified product page test, adding parameterization and waiting for a fall

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,22 @@
+import pytest
 from .pages.product_page import ProductPage
 
 
-def test_add_to_basket_promo_product(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# @pytest.mark.parametrize('links', [i if i != 7 else pytest.param(i, marks=pytest.mark.xfail) for i in range(10)])
+@pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+                                                "/?promo=offer7", marks=pytest.mark.xfail),
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, links):
+    link = links  # f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{links}"
+                  #"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket_promo_product()
